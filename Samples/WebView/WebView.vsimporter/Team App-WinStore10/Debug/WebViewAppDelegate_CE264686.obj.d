33b97fa85f6d1bd1fd6a72fc43d1da12488a9d75
Carlosgm02 <33579307+Carlosgm02@users.noreply.github.com>
2018-11-03 09:47:20 -0600

    Add files via upload

---
+++ Samples/WebView/WebView.vsimporter/Team App-WinStore10/Debug/WebViewAppDelegate_CE264686.obj.d
@@ -0,0 +1,6 @@
+C:\Users\carlo\source\objc\Apps\WebView\WebView.vsimporter\Team\ App-WinStore10\Debug\WebViewAppDelegate_CE264686.obj: \
+  C:\Users\carlo\source\objc\Apps\WebView\Classes\WebViewAppDelegate.m \
+  C:\Users\carlo\Downloads\iOS-Converter2\msvc\\objcrt\objc_2stage_init.h \
+  C:\Users\carlo\source\objc\Apps\WebView\WebView_Prefix.pch \
+  C:\Users\carlo\source\objc\Apps\WebView\Classes/WebViewAppDelegate.h \
+  C:\Users\carlo\source\objc\Apps\WebView\Classes/WebViewController.h
---
+++ Samples/WebView/WebView.vsimporter/Team App-WinStore10/Debug/WebViewController_43F54F80.obj.d
@@ -0,0 +1,5 @@
+C:\Users\carlo\source\objc\Apps\WebView\WebView.vsimporter\Team\ App-WinStore10\Debug\WebViewController_43F54F80.obj: \
+  C:\Users\carlo\source\objc\Apps\WebView\Classes\WebViewController.m \
+  C:\Users\carlo\Downloads\iOS-Converter2\msvc\\objcrt\objc_2stage_init.h \
+  C:\Users\carlo\source\objc\Apps\WebView\WebView_Prefix.pch \
+  C:\Users\carlo\source\objc\Apps\WebView\Classes/WebViewController.h
