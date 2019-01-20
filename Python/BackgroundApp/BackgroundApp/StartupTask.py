33a5969d432786abecf6b7cd9b78f6bb3fbb9c5d
Carlos González <33579307+Carlosgm02@users.noreply.github.com>
2019-01-19 22:15:13 -0600

    Update StartupTask.py

--- Python/BackgroundApp/BackgroundApp/StartupTask.py
+++ Python/BackgroundApp/BackgroundApp/StartupTask.py
@@ -1,1 +1,31 @@
-# Add your code here to run in your startup task
+# This is the code which will run in the startup task
+
+# Basic Print
+print("New Background UWP App here!")
+
+# This is Windows IoT
+mystring = 'Windows'
+print(mystring)
+mystring = "10 IoT"
+print(mystring)
+
+# One + Two = Three
+one = 1
+two = 2
+three = one + two
+print(three)
+
+# The classic Hello World
+hello = "hello"
+world = "world"
+helloworld = hello + " " + world
+print(helloworld)
+
+# 3 and 4 print
+a, b = 3, 4
+print(a,b)
+
+# Another one...
+five = 5
+six = 6
+print(five + six)
