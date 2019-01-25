8e31974252b8db3b121309cf8b3b1c67cde55899
Carlos González <33579307+Carlosgm02@users.noreply.github.com>
2019-01-25 17:03:28 -0600

    Add files via upload

---
+++ C#/ConsoleApp/ConsoleApp/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+// This example code shows how you could implement the required main function for a 
+// Console UWP Application. You can replace all the code inside Main with your own custom code.
+
+// You should also change the Alias value in the AppExecutionAlias Extension in the 
+// Package.appxmanifest to a value that you define. To edit this file manually, right-click
+// it in Solution Explorer and select View Code, or open it with the XML Editor.
+
+namespace ConsoleApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            if (args.Length == 0)
+            {
+                Console.WriteLine("Hello console! - no args");
+            }
+            else
+            {
+                for (int i = 0; i < args.Length; i++)
+                {
+                    Console.WriteLine($"arg[{i}] = {args[i]}");
+                }
+            }
+            Console.WriteLine("Press a key to continue: ");
+            Console.ReadLine();
+        }
+    }
+}
