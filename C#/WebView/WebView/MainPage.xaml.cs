f1d14df27676585000ebd5b928ce9f59174662d9
Carlos González <33579307+Carlosgm02@users.noreply.github.com>
2019-02-02 16:23:53 -0600

    Fooz the foo

---
+++ C#/WebView/WebView/MainPage.xaml.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.ApplicationModel.Core;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.Media.Core;
+using Windows.UI.Core;
+using Windows.UI.Popups;
+using Windows.UI.ViewManagement;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+
+// The Blank Page item template is documented at http://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x409
+
+namespace WebView
+{
+    /// <summary>
+    /// An empty page that can be used on its own or navigated to within a Frame.
+    /// </summary>
+    public sealed partial class MainPage : Page
+    {
+        public MainPage()
+        {
+            this.InitializeComponent();
+            WebView.ContainsFullScreenElementChanged += WebView_ContainsFullScreenElementChanged;
+            // WebView.NavigationStarting += WebView_NavigationStarting;
+            SystemNavigationManager.GetForCurrentView().BackRequested += SystemNavigationManager_BackRequested;
+        }
+
+        private void WebView_ContainsFullScreenElementChanged(WebView sender, object args)
+        {
+            var applicationView = ApplicationView.GetForCurrentView();
+
+            if (sender.ContainsFullScreenElement)
+            {
+                applicationView.TryEnterFullScreenMode();
+            }
+            else if (applicationView.IsFullScreenMode)
+            {
+                applicationView.ExitFullScreenMode();
+            }
+        }
+
+    /*  private async void Button_Click(object sender, RoutedEventArgs e)
+        {
+            string functionString = String.Format("document.getElementById('nameDiv').innerText = 'Hello, {0}';", nameTextBox.Text);
+            await WebView.InvokeScriptAsync("eval", new string[] { functionString });
+        }  */
+
+        private void SystemNavigationManager_BackRequested(object sender, BackRequestedEventArgs e)
+        {
+            Frame frame = Window.Current.Content as Frame;
+
+            if (frame.CanGoBack || WebView.CanGoBack)
+            {
+                e.Handled = true;
+
+                if (WebView.CanGoBack)
+                    WebView.GoBack();
+
+
+                if (frame.CanGoBack)
+                    frame.GoBack();
+            }
+        }
+
+        private void WebView_NavigationCompleted(WebView sender, WebViewNavigationCompletedEventArgs args)
+        {
+
+            SystemNavigationManager.GetForCurrentView().AppViewBackButtonVisibility =
+                    WebView.CanGoBack ?
+                    AppViewBackButtonVisibility.Visible :
+                    AppViewBackButtonVisibility.Collapsed;
+        }
+    }
+}
