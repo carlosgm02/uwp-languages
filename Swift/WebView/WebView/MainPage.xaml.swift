4626225683709fd98d70e541a8f60d8120f602d9
Carlos González <33579307+Carlosgm02@users.noreply.github.com>
2019-02-02 12:20:18 -0600

    Replaced files via upload

--- Swift/WebView/WebView/MainPage.xaml.swift
+++ Swift/WebView/WebView/MainPage.xaml.swift
@@ -1,13 +1,19 @@
 ﻿import System
 import System.Collections.Generic
+import System.ComponentModel
 import System.IO
 import System.Linq
 import System.Runtime.InteropServices.WindowsRuntime
+import Windows.ApplicationModel.Core
 import Windows.Foundation
 import Windows.Foundation.Collections
+import Windows.Media.Core
 import Windows.UI.Xaml
 import Windows.UI.Xaml.Controls
 import Windows.UI.Xaml.Controls.Primitives
+import Windows.UI.Core
+import Windows.UI.Popups
+import Windows.UI.ViewManagement
 import Windows.UI.Xaml.Data
 import Windows.UI.Xaml.Input
 import Windows.UI.Xaml.Media
@@ -23,6 +29,49 @@ internal __partial class MainPage : Page {
 	public init() {
 
 		self.InitializeComponent()
-      
+        // WebView.NavigationStarting += WebView_NavigationStarting
+        WebView.ContainsFullScreenElementChanged += WebView_ContainsFullScreenElementChanged
+		//WebView.NavigationStarting += WebView_NavigationStarting
+		SystemNavigationManager.GetForCurrentView().BackRequested += SystemNavigationManager_BackRequested
+
 	}
+
+    private func WebView_ContainsFullScreenElementChanged(_ sender: WebView!, _ args: Object!) {
+		var applicationView = ApplicationView.GetForCurrentView()
+		if sender.ContainsFullScreenElement {
+			applicationView.TryEnterFullScreenMode()
+		} else {
+			if applicationView.IsFullScreenMode {
+				applicationView.ExitFullScreenMode()
+			}
+		}
+	}
+
+    /*  private func Button_Click(_ sender: Object!, _ e: RoutedEventArgs!) {
+		var functionString: String! = String.Format("document.getElementById(\'nameDiv\').innerText = \'Hello, {0}\';", nameTextBox.Text)
+		__await webView1.InvokeScriptAsync("eval", ([functionString] as? String![]))
+    }  */
+
+    /*  private func WebView_NavigationStarting(_ sender: WebView!, _ args: WebViewNavigationStartingEventArgs!) {
+		if args.Uri.Host == "www.elementscompiler.com" {
+			WebView.AddWebAllowedObject("nativeObject", MyClass())
+		}
+    }  */
+
+    private func SystemNavigationManager_BackRequested(_ sender: Object!, _ e: BackRequestedEventArgs!) {
+		var frame: Frame! = (Window.Current.Content as? Frame)
+		if frame.CanGoBack | WebView.CanGoBack {
+			e.Handled = true
+			if WebView.CanGoBack {
+				WebView.GoBack()
+			}
+			if frame.CanGoBack {
+				frame.GoBack()
+			}
+		}
+    }
+
+    private func WebView_NavigationCompleted(_ sender: WebView!, _ args: WebViewNavigationCompletedEventArgs!) {
+		SystemNavigationManager.GetForCurrentView().AppViewBackButtonVisibility = (WebView.CanGoBack ? AppViewBackButtonVisibility.Visible : AppViewBackButtonVisibility.Collapsed)
+    }
 }
