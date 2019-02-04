ec66c236150d01d84f5af7b56162bdd4fa497ceb
Carlos González <33579307+Carlosgm02@users.noreply.github.com>
2019-02-04 13:42:01 -0600

    Delete main_13E16646.obj.d

--- Obj-C/WebView/WebView.vsimporter/Team App-WinStore10/Debug/main_13E16646.obj.d
+++
@@ -1,4 +0,0 @@
-C:\Users\carlo\source\objc\Apps\WebView\WebView.vsimporter\Team\ App-WinStore10\Debug\main_13E16646.obj: \
-  C:\Users\carlo\source\objc\Apps\WebView\main.m \
-  C:\Users\carlo\Downloads\iOS-Converter2\msvc\\objcrt\objc_2stage_init.h \
-  C:\Users\carlo\source\objc\Apps\WebView\WebView_Prefix.pch
